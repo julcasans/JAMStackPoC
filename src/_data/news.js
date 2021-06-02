8c069245be8d54f943151f460a5d9962f750af65
Julio E. Castillo Anselmi <castillo.julio@gmail.com>
2021-06-02 21:43:25 +0200

    add news for different countries

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,27 @@
 
 const axios = require('axios');
+const countries = require("./countries.json");
 require('dotenv').config();
 
-module.exports = async function() {
+async function getNews(country) {
   try {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`
     const response = await axios.get(url);
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles
+    }
   } catch(err) {
     console.error(err);
   }
+};
+
+module.exports = async function() {
+
+  const newsPromises = countries.map(getNews);
+
+  return Promise.all(newsPromises).then( newsObjects => {
+    console.log({newsObjects});
+    return [].concat.apply([], newsObjects);
+  })
 };
